ec408293192321522e476106abd0b50ef3d9bedc
Balthazar DOSSOU <balthazar.dossou@mehriso.com>
2023-05-12 11:34:25 +0100

    chore(): sanitize

--- src/index.ts
+++ src/index.ts
@@ -4,36 +4,51 @@ import { default as widget } from "./components/calendar/core-index.vue";
 import { i18n } from "./assets/i18n";
 
 // import the fontawesome core
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 // import font awesome icon component
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 // import specific icons
-import { faEye, faXmark, faChevronRight, faChevronLeft, faMagnifyingGlass, faPenNib, faCalendar } from '@fortawesome/free-solid-svg-icons'
+import {
+  faEye,
+  faXmark,
+  faChevronRight,
+  faChevronLeft,
+  faMagnifyingGlass,
+  faPenNib,
+  faCalendar,
+} from "@fortawesome/free-solid-svg-icons";
 
 // add icons to the library
-library.add(faEye, faXmark, faChevronRight, faChevronLeft, faMagnifyingGlass, faPenNib, faCalendar)
+library.add(
+  faEye,
+  faXmark,
+  faChevronRight,
+  faChevronLeft,
+  faMagnifyingGlass,
+  faPenNib,
+  faCalendar
+);
 
 const pinia = createPinia();
 
 const ProCalendar = {
-    install: (app: App<Element>) => {
-      app.use(pinia);
-      app.use(i18n);
+  install: (app: App<Element>) => {
+    app.use(pinia);
+    app.use(i18n);
 
+    app.provide("$t", app.config.globalProperties.$t);
+    app.provide("$i18n", app.config.globalProperties.$i18n);
+    app.provide("$locale", app.config.globalProperties.$i18n.locale);
 
-      app.provide("$t", app.config.globalProperties.$t);
-      app.provide("$i18n", app.config.globalProperties.$i18n);
-      app.provide("$locale", app.config.globalProperties.$i18n.locale);
+    app.component("font-awesome-icon", FontAwesomeIcon);
 
-      app.component('font-awesome-icon', FontAwesomeIcon)
-
-      // Plugin code goes here
-      app.component('ProCalendar', widget);
-    }
-}
+    // Plugin code goes here
+    app.component("ProCalendar", widget);
+  },
+};
 
 export { ProCalendar };
-export type { Appointment, T_View, T_Action, Configs } from './stores/events';
-export { E_View } from './stores/events';
+export type { Appointment, T_View, T_Action, Configs } from "./stores/events";
+export { E_View } from "./stores/events";
